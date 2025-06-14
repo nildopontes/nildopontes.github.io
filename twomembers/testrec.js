95cdfe9460448471581bd618834c3d6356be29ca
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2025-06-14 17:39:15 -0300

    Add files via upload

--- twomembers/testrec.js
+++ twomembers/testrec.js
@@ -1,140 +1,156 @@
-var chunks = [];
-var mediaRecorder;
-var eu;
+'use strict';
 
 const drone = new ScaleDrone('OOgX7u3om3pEfCPf');
 
-function downloadFile(blob){
-   let a = document.createElement('a');
-   a.style.display = 'none';
-   document.body.appendChild(a);
-   a.href = window.URL.createObjectURL(blob, {type: "audio/webm;codecs=opus"});
-   a.setAttribute('download', 'sound.opus');
-   a.click();
-   window.URL.revokeObjectURL(a.href);
-   document.body.removeChild(a);
-}
-
-// Cria o nome físico da sala, no scaledrone, por uma regra deles, deve ser precedido da string 'observable-'
-const roomName = 'observable-nildopontes';
 const configuration = {
-  iceServers: [{
-    urls: 'stun:stun.voipstunt.com'
-  }]
+   iceServers: [{
+      urls: 'stun:stun.l.google.com:19302'
+   }]
 };
-let room;
-let pc = new RTCPeerConnection(configuration);
-
-function onSuccess(msg) {
-    console.log(msg);
+const offerOptions = {
+  offerToReceiveAudio: 1,
+  offerToReceiveVideo: 0,
+  voiceActivityDetection: false
 };
-function onError(error) {
-  console.error(error);
-};
-
-// Evento disparado ao entrar no servidor de sinalização
-drone.on('open', error => {
-  if (error) {
-    return onError(error);
-  }
-  room = drone.subscribe(roomName);
-  room.on('open', error => {
-    if (error) {
-      onError(error);
-    }
-  });
-  // Se recebermos um array com pelo menos 2 membros, significa que a sala está pronta para iniciar a chamada.
-  room.on('members', members => {
-    // Trabalha com os nomes e quantidade de usuario aqui
-    onSuccess(members);
-    // Verifica se você é o primeiro ou segundo usuário, se for o segundo, então iniciamos a oferta de conexão
-    const isOfferer = members.length === 2;
-    eu = members.length;
-    startWebRTC(isOfferer);
-  });
+var showLog = false;
+var remoteAudio;
+var room;
+var pc = [new RTCPeerConnection(configuration), new RTCPeerConnection(configuration), new RTCPeerConnection(configuration)];
+document.addEventListener('DOMContentLoaded', function() {
+   remoteAudio = [remoteAudio1, remoteAudio2, remoteAudio3];
+   if(location.search == '?log'){
+      showLog = true;
+      log.style.display = 'initial';
+   }
+   onLog('Documento carregado');
 });
-
-// Envia uma mensagem pelo servidor de sinalização para todos os membros presentes na sala
-function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
-}
+var clients = [{id: ''}, {id: ''}, {id: ''}];
 
 function initStream(){
-   navigator.mediaDevices.getUserMedia({
-     audio: true,
-     video: false,
-   }).then(stream => {
-     // Envia o stream ao peer remoto
-     stream.getTracks().forEach(track => pc.addTrack(track, stream));
-   }, onError);
+   navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(stream => {
+      pc.forEach((element, index) => {
+         onLog('Fluxo local adicionado ao pc[' + index + ']');
+         element.addTrack(stream.getTracks()[0], stream);
+      });
+   });
 }
 
-function startWebRTC(isOfferer) {
-  // 'onicecandidate' é disparado sempre que o peer local encontra um novo icecandidate. A ação comum é enviar esse icecandidate para o peer remoto
-  pc.onicecandidate = event => {
-    if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
-    }
-  };
+pc.forEach((element, index) => {
+   element.onicecandidate = event => {
+      if(event.candidate){
+         onLog('Candidate enviado pelo pc[' + index + ']');
+         sendMessage({'candidate': event.candidate}, clients[index].id);
+      }
+   };
+   element.ontrack = event => {
+      const stream = event.streams[0];
+      onLog('Audio remoto recebido foi anexado à conexão em pc[' + index + ']');
+      remoteAudio[index].srcObject = stream;
+   };
+});
+function setAudioLayout(){
+   var qtdClients = 0;
+   clients.forEach((client, index) => {
+      if(client.id == ''){
+         remoteAudio[index].style.visibility = 'hidden';
+      }else{
+         qtdClients++;
+         remoteAudio[index].style.visibility = 'initial';
+      }
+   });
+   onLog(qtdClients + ' clientes online');
+}
+function onLog(msg){
+   if(!showLog) return;
+   log.value += msg + '\n';
+}
 
-  // Se é o segundo usuário, 'negotiationneeded' é criado e passa a oferecer a conexão local ao peer remoto
-  if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    }
-  }
-
-  // Quando recebemos a notificação de fluxo de áudio recebido, apresentamos ele dentro do elemento #remoteAudio
-  pc.ontrack = event => {
-    const stream = event.streams[0];
-    if (!remoteAudio.srcObject || remoteAudio.srcObject.id !== stream.id) {
-      remoteAudio.srcObject = stream;
-      mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-      mediaRecorder.onstop = e => {
-        //chunks.push(e.data);
-        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-        downloadFile(blob);
-      };
-      mediaRecorder.ondataavailable = e => {
-         console.log(1);
-        chunks.push(e.data);
-      };
-    }
-  };
-  // Fica escutando o Scaledrone para receber novas mensagens
-  room.on('data', (message, client) => {
-    // Encerra a função se a mensagem recebida foi enviada por mim
-    if (client.id === drone.clientId) {
-      onSuccess(message);
-      onSuccess(client);
+drone.on('open', error => {
+   if(error){
+      onLog(error);
       return;
-    }
+   }
+   room = drone.subscribe(roomName);
+   room.on('open', error => {
+      if(error){
+         onLog(error);
+      }
+   });
+   room.on('members', members => {
+      onLog('Entrei na sala com id = ' + drone.clientId + '. Usuarios online: ' + (members.length-1));
+      if(members.length > 1){
+         setAudioLayout();
+         members.forEach(member => {
+            if(member.id != drone.clientId){
+               for(var i = 0; i < 3; i++){
+                  if(clients[i].id === ''){
+                     clients[i].id = member.id;
+                     onLog('Cliente com id = ' + member.id + ' presente na sala, foi adicionado à lista local');
+                     break;
+                  }
+               }
+            }
+         });
+      }
+      setAudioLayout();
+      startWebRTC(members.length);
+   });
+   room.on('member_join', member => {
+      onLog('Um membro novo tentou entrar com id = ' + member.id);
+      for(var i = 0; i < 3; i++){
+         if(clients[i].id === ''){
+            clients[i].id = member.id;
+            onLog('Havia espaço. Ele conseguiu ficar.');
+            break;
+         }
+      }
+      setAudioLayout();
+   });
+   room.on('member_leave', member => {
+      onLog('Saiu um membro com id = ' + id);
+      const index = clients.findIndex(client => client.id === member.id);
+      clients[index].id = '';
+      setAudioLayout();
+   });
+});
 
-    if (message.sdp) {
-      // Quando recebemos a mensagem do outro cliente para fechar os pares
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // Respondemos a mensagem com nossos dados
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
-        }
-      }, onError);
-    } else if (message.candidate) {
-      // Adiciona o icecandidate recebido do peer remoto à conaxão local
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
-    }
-  });
+function sendMessage(message, destinyId){
+   if(destinyId == '') return;
+   message.destiny = destinyId;
+   onLog('Enviando para ' + message.destiny);
+   drone.publish({
+      room: roomName,
+      message
+   });
 }
-
-function localDescCreated(desc){
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
-  );
+function startWebRTC(qtdMembers){
+   onLog('startWebRTC(' + qtdMembers + ')');
+   if(qtdMembers > 1){
+      pc.forEach((element, index) => {
+         element.createOffer(offerOptions)
+                .then(offer => element.setLocalDescription(offer))
+                .then(() => {
+                   sendMessage({'sdp': element.localDescription}, clients[index].id);
+                   onLog('SDP enviado pelo pc[' + index + ']');
+                }).catch(err => onLog(err));
+      });
+   }
+   room.on('data', (message, client) => {
+      if(message.destiny != drone.clientId) return;
+      const index = clients.findIndex(member => member.id === client.id);
+      if(message.sdp){
+         onLog('SDP recebido de ' + client.id);
+         pc[index].setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+            if(pc[index].remoteDescription.type === 'offer'){
+               pc[index].createAnswer().then((offer) => pc[index].setLocalDescription(offer)).then(() => {
+                  sendMessage({'sdp': pc[index].localDescription}, clients[index].id); onLog('SDP enviado por pc[' + index + ']');}).catch((err) => {
+                  onLog(err);
+               });
+            }
+         }, onLog);
+      }else if(message.candidate){
+         onLog('Candidate recebido de ' + client.id);
+         pc[index].addIceCandidate(message.candidate).catch(err => onLog(err));
+      }
+   });
 }
