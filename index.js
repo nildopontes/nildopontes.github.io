b29cb6ac4c8da66e6b287b27f31d2ff7fbcafb15
Nildo Vieira Pontes <nildo.pontes@bol.com.br>
2025-06-16 17:59:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-window.addEventListener('resize', () => {
-   document.body.style.minHeight = `${window.innerHeight}px`;
-});
 window.addEventListener('load', () => {
    document.body.style.minHeight = `${window.innerHeight}px`;
    document.getElementById('year').innerText = new Date().getFullYear();
-   document.getElementsByClassName('foot')[0].style.display = 'block';
-   if(location.host == 'localhost:10000') eruda.init();
 });
 function detail(detail, src){
    if(confirm(detail)) window.location.href = src;
